8f94de29fcf4bd6396a21be56fcaffb60d3086b3
Andrew Malsbury <afm015@latech.edu>
2018-10-30 20:16:53 -0500

    10-30(Andrew)
    p6

---
+++ p6/06 A Fantastically Fabulous List-TEMPLATE.py
@@ -0,0 +1,25 @@
+###########################################################################################
+# Name: 
+# Date: 
+# Description: 
+###########################################################################################
+
+# function that:
+# (1) prompts the user for a list size
+# (2) prompts the user for the integers to store in the list (corresponding to the list size)
+# (3) creates the list
+# (4) returns the list
+
+
+
+###############################################
+# MAIN PART OF THE PROGRAM
+# implement the main part of your program below
+# comments have been added to assist you
+###############################################
+# create the list
+nums = fillList()
+
+# display information about the list using the list functions discussed in class
+# there is no need to write/call your own functions here
+
