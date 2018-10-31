e1451cf98d013b3af6cef6a14f4aaa1a737692b3
Abby <agg016@latech.edu>
2018-10-30 21:18:07 -0500

    10-30 THANKS FAM ILY

--- p6/06 A Fantastically Fabulous List-TEMPLATE.py
+++ p6/06 A Fantastically Fabulous List-TEMPLATE.py
@@ -1,15 +1,16 @@
 ###########################################################################################
-# Name: 
+# Name: Abby Gissendanner
 # Date: 
-# Description: 
+# Description: Make a list and sort it out
 ###########################################################################################
 
-# function that:
-# (1) prompts the user for a list size
-# (2) prompts the user for the integers to store in the list (corresponding to the list size)
-# (3) creates the list
-# (4) returns the list
 
+def listMaker ():
+    i = 0 #initializing the list size
+    size = int(input("How many integers would you like to add to the list? \n-> "))
+    while (i < size): 
+        theList.append(int(input("Enter an integer: ")))
+        i += 1
 
 
 ###############################################
@@ -18,7 +19,17 @@
 # comments have been added to assist you
 ###############################################
 # create the list
-nums = fillList()
+theList = [] #initializing the list itself
+listMaker()
+sortedlist = sorted(theList)
+print "The original list: " , theList
+print "The length of the list is: " , len(theList)
+print "The minimum value of the list is: " , sortedlist[0]
+print "The maximum value of the list is: " , sortedlist[-1]
+theList.reverse()
+print "The reversed list: " , theList
+print "The sorted list: " , sortedlist
+
 
 # display information about the list using the list functions discussed in class
 # there is no need to write/call your own functions here
