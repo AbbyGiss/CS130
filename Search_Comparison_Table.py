f8f134f19d68b98e2264962dd27dfc59ef42ca45
Abby <agg016@latech.edu>
2018-11-05 14:16:53 -0600

    updates

--- Search_Comparison_Table.py
+++ Search_Comparison_Table.py
@@ -8,37 +8,50 @@ import math
 #
 # a function that displays the table
 def tableMaker ():
-    print("_" * 30)
-    print("n\tSeq\tBin\tPerf")
-    print("_" * 30)
+    print ("_" * 30)
+    print ("n\tSeq\tBin\tPerf")
+    print ("_" * 30)
     n = minimum
-    while (n< maximum):
-        seqPerf = int (seqCalc(n))
-        binPerf = int (binCalc(n))
-        perfDiff = int (seqPerf/binPerf)
-        print ("%s\t%s\t%s\t%s"%(n, seqPerf, binPerf, perfDiff))
+    while (n < maximum):
+        seqPerf = int(seqCalc(n))
+        binPerf = int(binCalc(n))
+        perfNum = int(perfDiff(n))
+        print ("%s\t%s\t%s\t%s"%(n, seqPerf, binPerf, perfNum))
         n += interval
 #
 # a function that calculates the average number of comparisons of a sequential search on a list of size n
 # -input: the list size
 # -output: the average number of comparisons
 def seqCalc (size):
-    avrg = math.ceil(size/2)
-    return avrg
+    if size == 0:
+        return 0
+    if size > 0:
+        avrg = math.floor(size/2)
+        return avrg
 #
 # a function that calculates the maximum number of comparisons of a binary search on a list of size n
 # -input: the list size
 # -output: the average number of comparisons
 def binCalc (size):
-    avrg = math.floor(math.log(size,2)+1)
-    return avrg
+    if size == 0:
+        return 0
+    if size > 0:
+        avrg = math.ceil(math.log(size + 1,2))
+        return avrg
+#
+def perfDiff (size):
+    if size == 0:
+        return 0
+    if size > 0:
+        diff = int(seqCalc(size)/binCalc(size))
+        return diff
 #
 ###############################################
 # MAIN PART OF THE PROGRAM
 ###############################################
 # get user input for the minimum (make sure that it is >= 0)
 minimum = int (input ("Enter minimun value:\n-> "))
-while (minimum < 1):
+while (minimum < 0):
     minimum = int (input("!!!!!ERROR!!!!! Please enter a number that is >= 0:\n-> "))
 #   
 # get user input for the maximum (make sure that is is >= minimum)
