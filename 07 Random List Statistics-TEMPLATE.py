011babe8fd326230609d01134ffb2d9d4e1e3cb8
Abby <agg016@latech.edu>
2018-11-06 20:27:37 -0600

    changed name and moved the program to my computer science folder

---
+++ 07 Random List Statistics-TEMPLATE.py
@@ -0,0 +1,39 @@
+###########################################################################################
+# Name: 
+# Date: 
+# Description: 
+###########################################################################################
+
+# function that prompts the user for a list size, minimum and maximum values, creates the list, and returns it
+# you must use the list functions discussed in class to add integers to the list
+
+# function that receives the list as a parameter, and calculates and returns the mean
+
+
+# function that receives the list as a parameter, and calculates and returns the median
+
+
+# function that receives the list as a parameter, and calculates and returns the range
+
+
+
+###############################################
+# MAIN PART OF THE PROGRAM
+# implement the main part of your program below
+# comments have been added to assist you
+###############################################
+# create the list
+nums = fillList()
+
+# display the list
+# there is no need to write/call your own function for this part
+
+
+# calculate and display the mean
+
+
+# calculate and display the median
+
+
+# calculate and display the range
+
---
+++ Random_List_Stats.py
@@ -0,0 +1,43 @@
+###########################################################################################
+# Name: Abby Gissendanner
+# Date: 11-6-18
+# Description: Creates a random list using the max and min provided by the user 
+###########################################################################################
+
+# function that prompts the user for a list size, minimum and maximum values, creates the list, and returns it
+# you must use the list functions discussed in class to add integers to the list
+def listMaker ():
+    nums = [] #initializing the list 
+    size = int(input("How many integers would you like to add to the list? \n-->")) #getting the list size
+    while (nums < size):
+        numbers = 
+# function that receives the list as a parameter, and calculates and returns the mean
+
+
+# function that receives the list as a parameter, and calculates and returns the median
+
+
+# function that receives the list as a parameter, and calculates and returns the range
+
+
+
+###############################################
+# MAIN PART OF THE PROGRAM
+# implement the main part of your program below
+# comments have been added to assist you
+###############################################
+# create the list
+nums = fillList()
+
+# display the list
+# there is no need to write/call your own function for this part
+
+
+# calculate and display the mean
+
+
+# calculate and display the median
+
+
+# calculate and display the range
+
