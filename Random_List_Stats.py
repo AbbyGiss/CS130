e035b806193e505597926f10cf4f00f7a33c14ad
Abby <agg016@latech.edu>
2018-11-08 19:05:21 -0600

    RIP me this is killing me

--- Random_List_Stats.py
+++ Random_List_Stats.py
@@ -1,22 +1,39 @@
 ###########################################################################################
 # Name: Abby Gissendanner
 # Date: 11-6-18
-# Description: Creates a random list using the max and min provided by the user 
+# Description: Creates a random list using the max and min provided by the user and gives the statistics
 ###########################################################################################
 
 # function that prompts the user for a list size, minimum and maximum values, creates the list, and returns it
 # you must use the list functions discussed in class to add integers to the list
-def listMaker ():
-    nums = [] #initializing the list 
-    size = int(input("How many integers would you like to add to the list? \n-->")) #getting the list size
-    while (nums < size):
-        numbers = 
+import math
+def fillList ():
+        size = int(input("How many random integers would you like to add to the list? \n--> "))
+        minimum = int(input("What would you like the minimum value to be? \n--> "))
+        maximum = int(input("What would you like the maximum value to be? \n--> "))
+        from random import randint
+        numbers = []
+        for items in range (0, size):
+                num = randint(minimum, maximum)
+                numbers.append (num)
+        return numbers
 # function that receives the list as a parameter, and calculates and returns the mean
-
-
+def mean (nums):
+        sum_num = 0
+        for numbers in nums:
+                sum_num += numbers
+        return (sum_num / len(nums))
 # function that receives the list as a parameter, and calculates and returns the median
-
-
+def median (nums):
+        nums.sort ()
+        n = len(nums) / 2
+        if ((len(nums) * 2) == 0):
+                mid = (nums [n] + nums [n -1]) / 2
+        elif (len(nums) == 3):
+                mid =  nums [n] / 2
+        else:
+                mid = nums [int(math.ceil(n))]
+        return mid
 # function that receives the list as a parameter, and calculates and returns the range
 
 
@@ -30,14 +47,13 @@ def listMaker ():
 nums = fillList()
 
 # display the list
+print ("The list: {}".format(nums))
 # there is no need to write/call your own function for this part
-
-
 # calculate and display the mean
-
-
+mean = mean (nums)
+print ("The mean of the list is: {}".format(mean))
 # calculate and display the median
-
-
+med = median(nums)
+print ("The median of the list is: {}".format(med))
 # calculate and display the range
 
